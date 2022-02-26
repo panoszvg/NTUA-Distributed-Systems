9b87bce3156c9ecc99d0bf8f78b85584db635bdd
panoszvg <44406295+panoszvg@users.noreply.github.com>
2022-02-26 17:50:55 +0200

    Add basic class information and config file

--- block.py
+++ block.py
@@ -1,22 +1,40 @@
 
-import blockchain
+import json
+import datetime
+from Crypto.Hash import SHA256
 
 
 
 
 class Block:
-	def __init__(self):
-		##set
-
-		#self.previousHash
-		#self.timestamp
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
+	'''
+	Initialize a Block object with base information
+	'''
+	def __init__(self, previous_hash, index):
+		self.index = index
+		self.timestamp = datetime.datetime.now().timestamp()
+		self.transactions = []
+		self.nonce = 0
+		self.current_hash = None # will change after it's hashed
+		self.previous_hash = previous_hash
 	
-	def myHash:
-		#calculate self.hash
+	'''
+	Calculate current hash:
+	Create a dict with block's information and encrypt it
+	'''
+	def myHash(self):
+		block_info = json.dumps(dict(
+			index = self.index,
+			timestamp = self.timestamp,
+			transactions = self.transactions, # might need to unpack this
+			nonce = self.nonce,
+			previous_hash = self.previous_hash
+		))
+		return SHA256.new(block_info.encode()).hexdigest()
 
+	# '''
+	# Add a transaction to the block:
+	# Essentially append it to transactions list
+	# '''
+	# def add_transaction(transaction transaction):
 
-	def add_transaction(transaction transaction, blockchain blockchain):
-		#add a transaction to the block
--- transaction.py
+++ transaction.py
@@ -4,32 +4,57 @@ import binascii
 
 import Crypto
 import Crypto.Random
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
+import json
 import requests
 from flask import Flask, jsonify, request, render_template
 
 
 class Transaction:
 
-    def __init__(self, sender_address, sender_private_key, recipient_address, value):
+    '''
+    Initialize a Transaction object with necessary information:
 
+    Attributes
+    ----------
+    sender_address: str
+        public key of the sender's wallet
+    receiver_address: str
+        public key of receiver's wallet
+    amount: int
+        amount of NBC to be transferred
+    transaction_id: str
+        hash string of the transaction - using parameters above
+    transaction_inputs: list(str)
+        list that contains transactions ids as inputs
+    transaction_outputs: list(str)
+        list that contains UTXOs
+    '''
+    def __init__(self, sender_address, receiver_address, amount, transaction_inputs):
+        self.sender_address = sender_address # To public key του wallet από το οποίο προέρχονται τα χρήματα
+        self.receiver_address = receiver_address # To public key του wallet στο οποίο θα καταλήξουν τα χρήματα
+        self.amount = amount # το ποσό που θα μεταφερθεί
+        self.transaction_id = self.get_hash() # το hash του transaction
+        self.transaction_inputs = transaction_inputs # λίστα από Transaction Input 
+        self.transaction_outputs = [] # λίστα από Transaction Output 
 
-        ##set
-
-
-
-        #self.sender_address: To public key του wallet από το οποίο προέρχονται τα χρήματα
-        #self.receiver_address: To public key του wallet στο οποίο θα καταλήξουν τα χρήματα
-        #self.amount: το ποσό που θα μεταφερθεί
-        #self.transaction_id: το hash του transaction
-        #self.transaction_inputs: λίστα από Transaction Input 
-        #self.transaction_outputs: λίστα από Transaction Output 
-        #selfSignature
-
-
+    '''
+    Function that returns hash string used as transaction_id,
+    using transaction information 
+    
+    return: str
+    '''
+    def get_hash(self):
+        transaction_info = json.dumps(dict(
+            sender_address = self.sender_address,
+            receiver_address = self.receiver_address,
+            amount = self.amount,
+            transaction_inputs = self.transaction_inputs
+        ))
+        return SHA256.new(transaction_info.encode()).hexdigest()
     
 
 
